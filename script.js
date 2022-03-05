bafad5fc76f84cbd79e854056393ef3be15dc7f2
steph-kimm <47402894+steph-kimm@users.noreply.github.com>
2022-03-05 16:35:22 -0500

    design and features added

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 let petName = ""
 // Goal is that Tomodatchi is a class and the start makes the pet. 
 
-
-
 let button = document.querySelector("button")
 button.addEventListener("click" , function(event){
     event.preventDefault()
@@ -14,9 +12,12 @@ button.addEventListener("click" , function(event){
     document.getElementById("title").innerHTML = name
     console.log(document.getElementById(name))
    // window.location.href = "game.html";
+    
+    document.querySelector("form").style.display = "none";
     animateAge()
     animateEnergy()
     animateHunger()
+    animateBoredom()
 })
 
 
@@ -27,7 +28,11 @@ function animateAge(){
     setInterval(function(){
         document.getElementById("age").innerHTML = "Age: " + Agetime + " "
         Agetime++ 
-    }, 1000) //goes up every second right now 
+        //switch to adult pic 
+        if(Agetime == 20)document.getElementById("petPicture").setAttribute("src", "green-adult-dino.jpeg")
+
+        setStatus();
+    }, 5000) //goes up every second right now 
 }
 
 let Energytime = 100
@@ -38,29 +43,66 @@ function animateEnergy(){
         document.getElementById("energy").innerHTML = "Energy: " +Energytime + " %"
         //document.getElementById("energyNum").innerHTML = time + 
         Energytime-- 
-    }, 1000) //goes up every second right now 
+    }, 4000) //goes up every second right now 
 }
 
 function sleep(){
     console.log("Sleeping")
     Energytime+= 10;
+    setStatus()
     document.getElementById("energy").innerHTML = "Energy: " + (Energytime) + " %"
 }
 
 
 let feedTime = 10
 function animateHunger(){
-    
-    document.getElementById("energy").innerHTML = "Energy: " +feedTime + " %"
+    document.getElementById("hunger").innerHTML = "Hunger: " +feedTime + " %"
     setInterval(function(){
         document.getElementById("hunger").innerHTML = "Hunger: " +feedTime + " %"
         //document.getElementById("energyNum").innerHTML = time + 
         feedTime++
-    }, 1000) //goes up every second right now 
+    }, 2000) //goes up every second right now 
 }
 
 function feed(){
     feedTime-= 10;
+    setStatus()
     document.getElementById("hunger").innerHTML = "Hunger: " + (feedTime) + " %"
 
+}
+
+
+
+let boreTime = 15; 
+function animateBoredom(){
+    
+    document.getElementById("boredom").innerHTML = "Boredom: " +boreTime + " %"
+    setInterval(function(){
+        document.getElementById("boredom").innerHTML = "Boredom: " +boreTime + " %"
+        //document.getElementById("energyNum").innerHTML = time + 
+        boreTime++
+    }, 3000) //goes up every second right now 
+}
+
+function boredom(){
+    boreTime -= 10;
+    setStatus()
+    document.getElementById("boredom").innerHTML = "Boredom: " + (boreTime) + " %"
+}
+
+
+function setStatus(){
+    if(Energytime < 0 || Agetime > 30 || feedTime > 100 || boreTime > 100){
+        document.getElementById("statusPic").setAttribute("src" , "statusPics/dead.png")
+        document.getElementById("petPicture").setAttribute("src" , "statusPics/bones.jpeg")
+    }else if(boreTime>50){
+        document.getElementById("statusPic").setAttribute("src" , "statusPics/bored.jpeg")
+    }else if(feedTime>40){
+        document.getElementById("statusPic").setAttribute("src" , "statusPics/hungry.jpeg")
+    }else if(Energytime<30){
+        document.getElementById("statusPic").setAttribute("src" , "statusPics/sleepy.jpeg")
+    }else{
+        document.getElementById("statusPic").setAttribute("src" , "statusPics/happy.jpeg")
+    }
+    
 }
